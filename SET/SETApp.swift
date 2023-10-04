39214608f2a579fc6782782e91db14fb4bb63008
Naomi Anderson <ms.naomi.anderson@gmail.com>
2023-10-04 19:32:56 -0400

    Initial Commit

---
+++ SET/SETApp.swift
@@ -0,0 +1,17 @@
+//
+//  SETApp.swift
+//  SET
+//
+//  Created by Naomi Anderson on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
